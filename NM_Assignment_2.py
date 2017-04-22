c942108505298496c919dcf671bc9e760bf990ad
balaji-sridhar <balaji.subramanian@ucdconnect.ie>
2017-04-23 00:19:50 +0100

    Added feature to track the simulatio properties
    Included few more meta data

--- NM_Assignment_2.py
+++ NM_Assignment_2.py
@@ -9,8 +9,10 @@
 import networkx as nx
 import random
 import json
+import csv
 import logging as log
 from networkx.readwrite import json_graph
+import itertools
 
 
 def load_standford_graph(dataFile):
@@ -27,37 +29,36 @@ def load_standford_graph(dataFile):
         log.debug("%s , %s", nodeset[0], nodeset[1])
         # Verify if the nodes exist in the graph, if not add it
         if not G1.has_node(nodeset[0]):
-            G1.add_node(nodeset[0])
+            node_props = load_node_properties()
+            G1.add_node(nodeset[0], node_props)
+
         if not G1.has_node(nodeset[1]):
-            G1.add_node(nodeset[1])
+            node_props = load_node_properties()
+            G1.add_node(nodeset[1], node_props)
 
         # Create the edge from node[0] to node[1]
         G1.add_edge(nodeset[0], nodeset[1])
+        # Updating the node properties
+        G1.node[nodeset[0]]['references'] = G1.node[nodeset[0]]['references'] + 1
+        G1.node[nodeset[1]]['cited_by'] = G1.node[nodeset[1]]['cited_by'] + 1
+
     log.info("Number of nodes and edges in the graph are : %s , %s", G1.number_of_nodes(), G1.number_of_edges())
+    write_graph_in_json(G1, 'stanford_dataset.json')
     return G1
 
 
 def get_graph_properties(G):
-    # TODO - to verify how SCC an WCC are calculated. At the moment all the nodes are considered as SCC
     # Print the graph properties, to be used for analysis
     log.info("Analyzing the graph : ")
     graph_properties = {}
     graph_scc = nx.number_strongly_connected_components(G)
-    '''
-        print("Stongly connected components are ")
-        for i in nx.strongly_connected_components(G):
-            print(i)
-    
-        print("Weakly connected components are ")
-        for i in nx.weakly_connected_components(G):
-            print(i)
-    '''
     graph_properties["scc"] = graph_scc
     log.debug("Graph SCC is: %s", graph_scc)
     graph_wcc = nx.number_weakly_connected_components(G)
     graph_properties["wcc"] = graph_wcc
     log.debug("Graph WCC is: %s", graph_wcc)
     # print("Value of the graph properties: ", graph_properties)
+    # TODO: Update the graph properties, to cater the new simulation properties
     return graph_properties
 
 
@@ -71,7 +72,7 @@ def create_graph_organically(no_of_nodes):
         if i < 5:
             G2.add_node(i, node_props)
         else:
-            add_nodes_in_timestep(G2, 1)
+            add_nodes_in_timestep(G2, 1, 1, 0)
     log.info("Number of nodes and edges in the graph  --- %s , %s", G2.number_of_nodes(), G2.number_of_edges())
 
     # Removing self loop in the graph
@@ -83,28 +84,58 @@ def create_graph_organically(no_of_nodes):
         print(iter[0])
         G2.remove_edge(iter[0], iter[0])
     log.debug("Aft: %s ", G2.edges())
-    # Writing graph in edgelist format with no node
-    nx.write_edgelist(G2, 'test.edgelist', data=False)
-    write_graph_in_json(G2)  # To be enabled only for small graph <10000 nodes
+    write_graph_in_json(G2, 'node_link_data.json')
     graph_properties = get_graph_properties(G2)
     log.info("Value of the graph properties: %s ", graph_properties)
     return G2
 
 
-def add_nodes_in_timestep(G, no_of_time_steps):
-    node_counter = G.number_of_nodes() + 1
+def print_simulation_metrics(G, filename):
+    # Print the simulation properties to csv file
+    # no:of nodes in graph, no:of nodes in new status, no:of nodes in moderate status, no:of nodes in famouos status , no:of nodes in influential status
+    total_no_of_nodes = G.number_of_nodes()
+    nodes_in_new_status = len(get_nodes_from_graph(G, 'popularity_level', 'new'))
+    nodes_in_moderate_status = len(get_nodes_from_graph(G, 'popularity_level', 'moderate'))
+    nodes_in_famous_status = len(get_nodes_from_graph(G, 'popularity_level', 'famous'))
+    nodes_in_influential_status = len(get_nodes_from_graph(G, 'popularity_level', 'influential'))
+    if(filename != ""):
+        fd = open(filename, 'w+')
+        writer = csv.writer(fd)
+        writer.writerow([total_no_of_nodes, nodes_in_new_status, nodes_in_moderate_status, nodes_in_famous_status,
+                 nodes_in_influential_status])
+        fd.close()
+
+
+def add_nodes_in_timestep(G, no_of_time_steps, with_edges, with_metrics, filename=""):
+    if G.number_of_nodes() > 0:
+        node_counter = G.number_of_nodes() + 1
+    else:
+        node_counter = 0
     for i in range(node_counter, node_counter + no_of_time_steps):
         node_props = load_node_properties()
         G.add_node(i, node_props)
         s = i
-        add_citations_with_filter(G, s, i)
+        if with_edges == 1:
+            add_citations_with_filter(G, s, i)
 
+    if with_metrics == 1:
+        # Iterate over all the nodes and update the node status
+        indegree_centralities = nx.in_degree_centrality(G)
+        for n, c in indegree_centralities.items():
+            # Check the centrality range and update the popularity state
+            if c > 0 and c <= 0.15:
+                G.node[n]["popularity_level"] = "moderate"
+            elif c > 0.15 and c <= 0.30:
+                G.node[n]["popularity_level"] = "famous"
+            elif c > 0.30:
+                G.node[n]["popularity_level"] = "influential"
+        print_simulation_metrics(G, filename)
 
-def write_graph_in_json(G):
+def write_graph_in_json(G, filename):
     # Writing generated graph to the file in json format
     data = json_graph.node_link_data(G)
-    nx.write_edgelist(G, "edgelist_graph")
-    f = open('node_link_data.json', 'w')
+    # nx.write_edgelist(G, "edgelist_graph")
+    f = open(filename, 'w')
     # f.write(data)
     json.dump(data, f)
 
@@ -170,7 +201,7 @@ def add_citations_with_filter(G2, paper, no_of_nodes):
             G2.add_edges_from([(paper, reference)])
             # Now update the meta data of the nodes
             G2.node[paper]['references'] = G2.node[paper]['references'] + 1
-            G2.node[reference]['referenced_by'] = G2.node[reference]['referenced_by'] + 1
+            G2.node[reference]['cited_by'] = G2.node[reference]['cited_by'] + 1
             if G2.node[reference]['first_cited_time'] == 0:
                 G2.node[reference]['first_cited_time'] = paper
         # Making sure the graph is acyclic
@@ -187,29 +218,71 @@ def add_citations_with_filter(G2, paper, no_of_nodes):
 def load_node_properties():
     group_values = ["science", "fictional", "comic"]
     sub_group_values = ["neuro", "physics", "chemistry", "maths"]
+    popularity_level = ["new", "moderate", "famous", "influential"]
     references = 0  # number of papers it has cited or number of outbound edges
-    referenced_by = 0  # number of papers which has cited this paper or no:of inbound edges
+    cited_by = 0  # number of papers which has cited this paper or no:of inbound edges
     first_cited_time = 0  # Represent the number of time step at which the first citation happened
     node_props = {"group": random.choice(group_values), "subgroup": random.choice(sub_group_values),
-                  "probability": random.uniform(0, 0.5), "references": references, "referenced_by": referenced_by,
-                  "first_cited_time": first_cited_time}
+                  "probability": random.uniform(0, 0.5), "references": references, "cited_by": cited_by,
+                  "first_cited_time": first_cited_time, "popularity_level": popularity_level[0]}
     log.debug("Filtering criteria is %s ", node_props)
     return node_props
+
+
+def generate_erdos_renyi_graph(n, p, seed=None):
+    """
+        This function generates graph of node n with random number of edges, with probability p
+        
+        NOTE:This function implementation is inspired from networkx implementaiton nx.gnp_random_graph()and being modified 
+        to cater the needs of this simulation. We are overriding the function, so meta data can be updated.
+            
+    """
+    G = nx.DiGraph()
+    # Replace this function with node addition
+    add_nodes_in_timestep(G, n, 0, 0)
+
+    G.name = "gnp_random_graph(%s,%s)" % (n, p)
+
+    if p <= 0:
+        return G
+
+    if not seed is None:
+        random.seed(seed)
+
+    if G.is_directed():
+        edges = itertools.permutations(range(n), 2)
+    else:
+        edges = itertools.combinations(range(n), 2)
+
+    if p >= 1:
+        G.add_edges_from(edges)
+        return G
+
+    for e in edges:
+        if random.random() < p:
+            # print(e[0],e[1], e)
+            # Updating the meta data, to keep count of number of in bound and outbound edges
+            add_citations_with_filter(G, e[0], e[1])
+    write_graph_in_json(G, 'erdos_renyi.json')
+    return G
 
 
 if __name__ == "__main__":
     log.basicConfig(level=log.INFO)
 
     G = create_graph_organically(35)
-    # Update the timestep parameter to scale the number of nodes to be processed
-    add_nodes_in_timestep(G, 10)
+    # Update the time step parameter to scale the number of nodes to be processed
+    add_nodes_in_timestep(G, 10, 1, 0)
     graph_properties = get_graph_properties(G)
     log.info("Value of the home grown graph properties: %s ", graph_properties)
 
-    G1 = load_standford_graph("Cit-HepPh.txt")
+    G1 = load_standford_graph("test.edgelist")
     stanford_graph_properties = get_graph_properties(G1)
     log.info("Value of the stanford graph properties: %s ", stanford_graph_properties)
 
-    G2 = nx.gnm_random_graph(100, 250, directed=True)
+    G2 = generate_erdos_renyi_graph(100, 0.3)
     gnm_graph_properties = get_graph_properties(G)
     log.info("Value of the GNM  graph properties: %s ", gnm_graph_properties)
+    print(len(G2), nx.in_degree_centrality(G2))
+    add_nodes_in_timestep(G2, 50, 1, 1,'erdos_renyi_sim_metrics.csv')
+    add_nodes_in_timestep(G2, 50, 1, 1,'erdos_renyi_sim_metrics.csv')
